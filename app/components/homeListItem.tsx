a6ce4fd283d0941289eab2eaaffcfed93834dcf6
alex <boltonalex@gmail.com>
2024-11-01 18:01:28 +0000

    home-page restyle

---
+++ app/components/homeListItem.tsx
@@ -0,0 +1,26 @@
+interface HomeListItemProps {
+  count: number;
+  title: string;
+  description: string;
+}
+
+function HomeListItem({ count, title, description }: HomeListItemProps) {
+  return (
+    <li className='my-2 p-4 flex content-center flex-col md:flex-row md:items-center'>
+      <span
+        className="md:flex items-center justify-center w-16 h-16 text-white font-bold rounded-full mr-3 hidden "
+        style={{ background: 'rgb(86, 77, 62)' }}
+      >
+        {count}
+      </span>
+
+      <div className='flex flex-col md:items-center md:justify-center md:flex-row'>
+        <span className='flex font-bold md:w-[200px] text-wrap'>{title}</span>
+        <span className='flex md:max-w-96'>{description}</span>
+      </div>
+
+    </li>
+  )
+}
+
+export default HomeListItem
--- app/page.tsx
+++ app/page.tsx
@@ -1,56 +1,69 @@
 import Link from "next/link";
+import Image from "next/image";
+// import styles from './Home.module.css'; // Import the CSS module
+import HomeListItem from "./components/homeListItem";
 
 export default function Home() {
   return (
-    <div className='flex w-full md:justify-center'>
-      <div className="flex flex-col max-w-screen-lg items-center min-h-screen bg-gray-100 p-4">
-        <h1 className='flex text-2xl bg-slate-600 p-4 text-white w-full justify-center md:text-4xl md:pt-40 md:pb-20 '>
-          Welcome to the Homework Helper!
-        </h1>
-        <p className='p-4'>
+    <div className='flex w-full md:justify-center' style={{ background: 'rgb(220, 208, 183)' }}>
+      <div className="flex flex-col max-w-screen-lg items-center min-h-screen p-4">
+        <div className="heroImage flex text-black w-full h-[400px] md:h-[600px] justify-center md:text-4xl pt-[310px] md:pt-[500px]">
+          <h1 className="text-2xl md:text-5xl font-extrabold md:h-[1em]">
+            Welcome to the Homework Helper!
+          </h1>
+        </div>
+        <p className='pb-4'>
           Our website is a learning aid for parents supporting their child&lsquo;s primary education (Years 1-6) based on the UK National Curriculum. Here, you&lsquo;ll find clear explanations of key topics in Mathematics, English, and Science, organized by year and key stage, with guided methods, interactive activities, and soon we&lsquo;ll have visuals to make learning engaging and effective.
         </p>
-        <Link className="border-2 rounded border-blue-700 text-white bg-slate-600 m-4 p-2" href="/yearSelect">Choose a class</Link>
-        <h2 className='text-xl'>
-          Who is the Website For?
-        </h2>
-        <p className='p-4'>
-          This website is for parents and guardians looking to enhance their child&lsquo;s learning at home—without needing to sift through lengthy curriculum documents. Whether you&lsquo;re helping with a tricky Maths problem, working on English reading comprehension, or exploring Science concepts, our resources offer quick, clear guidance to support and motivate your child&lsquo;s learning journey.
-        </p>
 
-        <h2 className='text-xl'>
-          How to Use the Website
+        <Link href="/yearSelect" className='rounded text-white bg-slate-600 m-4 p-4' style={{ background: 'rgb(238, 138, 128)' }}>Get Started!</Link>
+
+        <div className='flex my-4 flex-col md:flex-row'>
+          <div className='flex content-center justify-center flex-col'>
+            <h2 className='text-xl font-bold'>
+              Who is the Website For?
+            </h2>
+            <p className='py-4'>
+              This website is for parents and guardians looking to enhance their child&lsquo;s learning at home—without needing to sift through lengthy curriculum documents. Whether you&lsquo;re helping with a tricky Maths problem, working on English reading comprehension, or exploring Science concepts, our resources offer quick, clear guidance to support and motivate your child&lsquo;s learning journey.
+            </p>
+          </div>
+          <Image src="/family.webp" alt="Parents helping child with homework" className='rounded-3xl' width={400} height={300} />
+        </div>
+
+        <h2 className='text-xl font-bold mt-4 w-full'>
+          How to use this resource
         </h2>
-        <p className='p-4'>
+        <p className='py-4 text-left w-full'>
           Our user-friendly interface is designed with ease and functionality in mind. Here&lsquo;s a step-by-step guide to exploring the resources:
         </p>
-        <ol className='list-decimal list-inside bg-slate-300 rounded-xl m-4'>
-          <li className='my-2 p-4'>
-            Select the Year Group: Start by choosing the year your child is currently in (Year 1 - Year 6).
-          </li>
-          <li className='my-2 p-4'>
-            Choose the Subject: Navigate through Mathematics, English, or Science, based on your focus.
-          </li>
-          <li className='my-2 p-4'>
-            Pick a Topic: Topics are divided according to the national curriculum for each year. Select a topic to view tailored explanations, activities, and soon we&lsquo;ll add visual aids.
-          </li>
-          <li className='my-2 p-4'>
-            Explore Interactive Activities: Each topic provides suggested activities that you can do together to strengthen understanding and engagement.
-          </li>
-          <li className='my-2 p-4'>
-            (Coming Soon) Access Visual Aids: Each topic includes a custom visual that illustrates key methods in a child-friendly, minimalist style. These visuals are perfect for reinforcing concepts in an easy-to-understand format.
-          </li>
+        <ol className='rounded-xl my-4' style={{ background: 'rgb(170, 186, 172)', color: 'rgb(86, 77, 62)' }}>
+          <HomeListItem count={1} title='Select the Year Group' description='Start by choosing the year your child is currently in (Year 1 - Year 6).' />
+          <HomeListItem count={2} title='Choose the Subject' description='Navigate through Mathematics, English, or Science, based on your focus.' />
+          <HomeListItem count={3} title='Pick a Topic' description='Topics are divided according to the national curriculum for each year. Select a topic to view tailored explanations, activities, and soon we&lsquo;ll add visual aids.' />
+          <HomeListItem count={4} title='Explore Interactive Activities' description='Each topic provides suggested activities that you can do together to strengthen understanding and engagement.' />
+          <HomeListItem count={5} title='(Coming Soon) Access Visual Guides' description='Each topic includes a custom visual that illustrates key methods in a child-friendly, minimalist style. These visuals are perfect for reinforcing concepts in an easy-to-understand format.' />
+
         </ol>
-        <h2 className='text-xl'>
-          Why Parents Love Our Website
+
+
+        <div className='flex my-4 flex-col md:flex-row'>
+          <Image src="/mother-son.webp" alt="Parents helping child with homework" className='rounded-3xl md:mr-8 mb-8 md:mb-0' width={400} height={300} />
+          <div className='flex content-center justify-center flex-col'>
+            <h2 className='text-xl font-bold'>
+              Why we made this Website
+            </h2>
+            <p className='py-4'>
+              As parents ourselves, we understand how challenging it can be to make home learning both effective and enjoyable. We created this website to save parents time by translating classroom methods from the curriculum into quick, easy-to-understand guides.
+            </p>
+            <p className='py-4'>
+              With these resources, you can dive right into helping with Maths, English, and Science, making learning at home less about deciphering school jargon and more about engaging activities that keep your child motivated and having fun.
+            </p>
+          </div>
+        </div>
+
+        <h2 className='text-xl font-bold'>
         </h2>
-        <p className='p-4'>
-          As parents ourselves, we understand how challenging it can be to make home learning both effective and enjoyable. We created this website to save parents time by translating classroom methods from the curriculum into quick, easy-to-understand guides.
-        </p>
-        <p className='p-4'>
-          With these resources, you can dive right into helping with Maths, English, and Science, making learning at home less about deciphering school jargon and more about engaging activities that keep your child motivated and having fun.
-        </p>
-        <Link className="border-2 rounded border-blue-700 text-white bg-slate-600 m-4 p-2" href="/yearSelect">Choose a class</Link>
+        <Link className='rounded text-white bg-slate-600 m-4 p-4' style={{ background: 'rgb(238, 138, 128)' }} href="/yearSelect">Get Started!</Link>
       </div>
     </div>
   );
